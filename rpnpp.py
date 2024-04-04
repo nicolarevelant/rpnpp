63e7b83cefce96e18a49328eddddb1e4ffd459b7
Nicola Revelant <nicolarevelant@outlook.com>
2024-04-04 18:41:51 +0200

    Add if command

--- rpnpp.py
+++ rpnpp.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
 
 import math
+import os
+import sys
+
+DEBUG = False
+if os.environ.get("RPNPP_DEBUG") == "1":
+    DEBUG = True
+elif "-d" in sys.argv:
+    DEBUG = True
 
 # Notazione polacca inversa con assegnazione variabili
 
 OPERATIONS = {
-        "+": [2, lambda x, y : x + y],
-        "-": [2, lambda x, y : x - y],
-        "*": [2, lambda x, y : x * y],
-        "/": [2, lambda x, y : x / y],
-        "**": [2, lambda x, y : x ** y],
-        "sin": [1, lambda x : math.sin(x)],
-        "cos": [1, lambda x : math.cos(x)],
-        "tan": [1, lambda x : math.tan(x)],
-        }
+        # arithmetic
+        "+": [lambda x, y : x + y, float, float],
+        "-": [lambda x, y : x - y, float, float],
+        "*": [lambda x, y : x * y, float, float],
+        "/": [lambda x, y : x / y, float, float],
+        "**": [lambda x, y : x ** y, float, float],
+        "sin": [lambda x : math.sin(x), float],
+        "cos": [lambda x : math.cos(x), float],
+        "tan": [lambda x : math.tan(x), float],
+
+        # logical
+        "<": [lambda x, y: x < y, float, float],
+        ">": [lambda x, y: x > y, float, float],
+        "<=": [lambda x, y: x <= y, float, float],
+        ">=": [lambda x, y: x >= y, float, float],
+        "==": [lambda x, y: x == y, float, float],
+
+        # condition
+        "if": [lambda x, y, cond: x if cond else y, None, None, bool],
+}
 
 variables = {}
 
@@ -24,56 +43,86 @@ def read_row():
     except EOFError:
         return None
 
+def try_float(value):
+    try:
+        return float(value)
+    except:
+        return None
+
+def try_bool(value):
+    if value.lower() == "true":
+        return True
+    if value.lower() == "false":
+        return False
+
 def main():
     isError = False
     stack = []
-    row = read_row()
-    while not isError and row is not None:
+    while not isError:
+        row = read_row()
+        if row is None:
+            break
+
         for value in row.split():
-            # decide if value is a number or a operator
-            try:
-                # number
-                num = float(value)
+            num = try_float(value)
+            if num is not None:
                 stack.append(num)
-            except ValueError:
-                # not a number
-                op = OPERATIONS.get(value)
-                if op is None:
-                    if value[0] == '$':
-                        # variable substitution
-                        num = variables.get(value[1:])
-                        if num is None:
-                            isError = True
-                            print(f" [EE] Variable not found: {value}")
+                continue
+            num = try_bool(value)
+            if num is not None:
+                stack.append(num)
+                continue
+            # not a number
+            op = OPERATIONS.get(value)
+            if op is not None:
+                # operation
+                # op[0]: lambda
+                # len(op) - 1: # of arguments
+                # op[1..len(op)-1]: arguments
+                args = []
+                if len(stack) >= len(op) - 1:
+                    for i in range(1, len(op)):
+                        arg = stack.pop()
+                        if op[-i] is None or type(arg) is op[-i]:
+                            args.insert(0, arg) # last removed item is the first argument
                         else:
-                            stack.append(num)
-                    elif len(stack) > 0:
-                        # variable assignment
-                        variables[value] = stack.pop()
-                    else:
-                        isError = True
-                        print(f" [EE] Empty stack while trying to fetch an item to be assigned to VAR: \"{value}\"")
-                else:
-                    # operation
-                    # [0]: 1 argument --> False, 2 arguments --> True
-                    # [1]: lambda
-                    args = []
-                    if len(stack) >= op[0]:
-                        for i in range(op[0]):
-                            args.insert(0, stack.pop())
-                        result = op[1](*args)
-                        
+                            isError = True
+                            print(f" [EE] Type mismatch: expected {op[i]} but got {type(arg)}")
+                            break
+
+                    if not isError:
+                        if DEBUG:
+                            print(f" [DD] OP: \"{value}\", args: \"{args}\"")
+                        result = op[0](*args)
                         stack.append(result)
                     else:
+                        break
+                else:
+                    isError = True
+                    print(f" [EE] Empty stack while trying to fetch arguments for OP: \"{value}\"")
+                    break
+            else:
+                # variables
+                if value[0] == '$':
+                    # variable substitution
+                    num = variables.get(value[1:])
+                    if num is None:
                         isError = True
-                        print(f" [EE] Empty stack while trying to fetch arguments for OP: \"{value}\"")
-        
-        if not isError:
-            row = read_row()
+                        print(f" [EE] Variable not found: {value}")
+                        break
+                    else:
+                        stack.append(num)
+                elif len(stack) > 0:
+                    # variable assignment
+                    variables[value] = stack.pop()
+                else:
+                    isError = True
+                    print(f" [EE] Empty stack while trying to fetch an item to be assigned to VAR: \"{value}\"")
+                    break
 
-    print("------------------------------------")
     print(stack)
-
+    return not isError and len(stack) == 1
 
 if __name__ == "__main__":
-    main()
+    if not main():
+        exit(1)
