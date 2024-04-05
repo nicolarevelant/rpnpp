eca785a11d2a2773c59fa74d2e215f98b5d06865
Nicola Revelant <nicolarevelant@outlook.com>
2024-04-05 15:09:44 +0200

    Print stack[0] if len(stack) == 1, and fix code style

--- rpnpp.py
+++ rpnpp.py
@@ -34,7 +34,6 @@ OPERATIONS = {
         "if": [lambda x, y, cond: x if cond else y, None, None, bool],
 }
 
-variables = {}
 
 def read_row():
     try:
@@ -55,9 +54,53 @@ def try_bool(value):
     if value.lower() == "false":
         return False
 
+def handleOperation(stack, opName, op):
+    # op[0]: lambda
+    # len(op) - 1: # of arguments
+    # op[1..len(op)-1]: arguments
+    if len(stack) < len(op) - 1:
+        print(f" [EE] Stack does not contain enough arguments for OP: \"{opName}\"")
+        return False
+
+    args = []
+    for i in range(1, len(op)):
+        arg = stack.pop()
+        if op[-i] is None or type(arg) is op[-i]:
+            args.insert(0, arg) # last removed item is the first argument
+        else:
+            isError = True
+            print(f" [EE] Type mismatch: expected \"{op[i]}\" but type({arg}) == \"{type(arg)}\"")
+            return False
+
+    if DEBUG:
+        print(f" [DD] Executing OP: {args} \"{opName}\"")
+
+    result = op[0](*args)
+    stack.append(result)
+
+    return True
+
+def handleVariables(stack, variables, value):
+    if value[0] == '$':
+        # variable substitution
+        num = variables.get(value[1:])
+        if num is None:
+            print(f" [EE] Variable not found: {value}")
+            return False
+        stack.append(num)
+    elif len(stack) > 0:
+        # variable assignment
+        variables[value] = stack.pop()
+    else:
+        print(f" [EE] Empty stack while trying to fetch an item to be assigned to VAR: \"{value}\"")
+        return False
+
+    return True
+
 def main():
     isError = False
     stack = []
+    variables = {}
     while not isError:
         row = read_row()
         if row is None:
@@ -72,56 +115,28 @@ def main():
             if num is not None:
                 stack.append(num)
                 continue
-            # not a number
+
             op = OPERATIONS.get(value)
             if op is not None:
                 # operation
-                # op[0]: lambda
-                # len(op) - 1: # of arguments
-                # op[1..len(op)-1]: arguments
-                args = []
-                if len(stack) >= len(op) - 1:
-                    for i in range(1, len(op)):
-                        arg = stack.pop()
-                        if op[-i] is None or type(arg) is op[-i]:
-                            args.insert(0, arg) # last removed item is the first argument
-                        else:
-                            isError = True
-                            print(f" [EE] Type mismatch: expected {op[i]} but got {type(arg)}")
-                            break
-
-                    if not isError:
-                        if DEBUG:
-                            print(f" [DD] OP: \"{value}\", args: \"{args}\"")
-                        result = op[0](*args)
-                        stack.append(result)
-                    else:
-                        break
-                else:
+                if not handleOperation(stack, value, op):
                     isError = True
-                    print(f" [EE] Empty stack while trying to fetch arguments for OP: \"{value}\"")
                     break
             else:
                 # variables
-                if value[0] == '$':
-                    # variable substitution
-                    num = variables.get(value[1:])
-                    if num is None:
-                        isError = True
-                        print(f" [EE] Variable not found: {value}")
-                        break
-                    else:
-                        stack.append(num)
-                elif len(stack) > 0:
-                    # variable assignment
-                    variables[value] = stack.pop()
-                else:
+                if not handleVariables(stack, variables, value):
                     isError = True
-                    print(f" [EE] Empty stack while trying to fetch an item to be assigned to VAR: \"{value}\"")
                     break
 
+        if DEBUG:
+            print(f" [DD] Stack: {stack}")
+
+    if len(stack) == 1:
+        print(stack[0])
+        return not isError
+
     print(stack)
-    return not isError and len(stack) == 1
+    return False
 
 if __name__ == "__main__":
     if not main():
